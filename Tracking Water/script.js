154206e0d3a38e92164186caabdd895167697085
Antorosales <129124164+Antorosales@users.noreply.github.com>
2023-05-02 15:45:17 -0400

    Pagina web

---
+++ Tracking Water/script.js
@@ -0,0 +1,12 @@
+function autocompletado () {
+    document.getElementById("busqueda").innerHTML = '';
+
+	var preguntame = [
+        {nombre: 'Agua Salada', descripcion: 2},
+        {nombre: 'Agua Dulce', descripcion: 0},
+        {nombre: 'Agua servida', descripcion: 5}
+    ];
+
+	
+	
+}
